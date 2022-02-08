96331bd16575c3600ec1ba60f933de2bafbf56aa
krapas170 <krapas170@gmail.com>
2022-02-08 09:53:52 +0100

    create table in database for gameserver

--- src/main/java/com/gameserverlive/configs/ServerConfig.java
+++ src/main/java/com/gameserverlive/configs/ServerConfig.java
@@ -1,10 +1,13 @@
 package com.gameserverlive.configs;
 
+import java.util.HashMap;
+
+import com.gameserverlive.Console;
 
 public class ServerConfig extends DefaultConfig {
 
     public ServerConfig() {
         super("server.json");
+        Console.info("gameserver.db geladen");
     }
-    
 }
